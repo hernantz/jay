5541c7eb53a8a39a401dac7a520366c110416505
Hernan Lozano <hernantz@gmail.com>
2013-11-17 17:49:07 -0300

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+requires = ['docopt', 'fuzzywuzzy', 'pyxdg']
+
+setup(
+    name='Jay',
+    version='0.1',
+    description='A command line tool for quickly jumping around your filesystem.',
+    license='MIT',
+    long_description='A command line tool for quickly jumping around your filesystem.',
+    author='hernantz',
+    author_email='hernantz@gmail.com',
+    installation_requires=requires,
+    scripts=['jay'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: MIT',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
