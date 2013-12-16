b4de8c093fe59eee9c58a33b4122cdf44e61e0ad
Hernan Lozano <hernantz@gmail.com>
2013-12-16 14:15:23 -0300

    Added tests for Jay.update method

--- test/tests.py
+++ test/tests.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import mock
+import jay
 sys.path.insert(0, os.path.abspath('..'))
 from jay import Jay
 from nose.tools import with_setup
@@ -40,7 +42,7 @@ def test_singleton():
 
 @with_setup(teardown=teardown_idx)
 def test_empty_idx_content_is_loaded_from_file():
-    """Idx file entries should be loaded as a dict"""
+    """An empty idx file should be loaded with no entries"""
     expected_rows = {}
     j = Jay(idx_filename=TEST_IDX_FILENAME)
     assert j.idx_rows == expected_rows
@@ -52,4 +54,34 @@ def test_idx_content_is_loaded_from_file():
     expected_rows = {'/tmp/dir1': '1387159989.41',
                      '/home/dir2': '1387158735.64'}
     j = Jay(idx_filename=TEST_IDX_FILENAME)
+    assert j.idx_rows == expected_rows
+
+
+@with_setup(teardown=teardown_idx)
+def test_update_with_new_directory():
+    """Jay.update should add the entry time for a new directory"""
+    assert not os.path.isfile(TEST_IDX_FILENAME)
+    d = '/test/dir'
+    update_time = '1387159989.41'
+    expected_rows = {d: update_time}
+    j = Jay(idx_filename=TEST_IDX_FILENAME)
+    with mock.patch.object(jay, 'time', return_value=update_time):
+        j.update(d)
+    assert j.idx_rows == expected_rows
+
+
+@with_setup(teardown=teardown_idx)
+def test_update_with_existing_directory():
+    """Jay.update should update the entry time of an existing entry"""
+    assert not os.path.isfile(TEST_IDX_FILENAME)
+    d = '/test/dir'
+    j = Jay(idx_filename=TEST_IDX_FILENAME)
+    update_time = '1387159989.41'
+    with mock.patch.object(jay, 'time', return_value=update_time):
+        j.update(d)
+
+    future_time = '2222222222.41'
+    expected_rows = {d: future_time}
+    with mock.patch.object(jay, 'time', return_value=future_time):
+        j.update(d)
     assert j.idx_rows == expected_rows
